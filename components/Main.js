92ca7d986893ffb64e477be2d86364a626ee9ae3
SimoSilve <163052000+SimoSilve@users.noreply.github.com>
2025-02-18 15:28:41 -0400

    Cambio de texto. nombre e imagenes

--- components/Main.js
+++ components/Main.js
@@ -14,10 +14,12 @@ const Main = (props) => (
         </article>
 
         <article id="work" className={`${props.article === 'work' ? 'active' : ''} ${props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src="/static/images/pic02.jpg" alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <h2 className="major">Servicios</h2>
+          <span className="image main"><img src="/static/images/img.jpg" alt="" /></span>
+          <p>➤ Servicio de flete internacional por contenedor 40/20 pies.</p>
+          <p>➤ Agenciamiento aduanal y nacionalizacion de carga.</p>
+          <p>➤ Importacion de China, EEUU, Panama a Venezuela.</p>
+          <p>➤ Despacho en todo el territorio nacional.</p>
           <div className="close" onClick={() => { props.onCloseArticle() }}></div>
         </article>
 
