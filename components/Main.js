5b4006752ade908e2314e3e7ec4c54ff5f00493f
Keeppz <kennethkipps25@gmail.com>
2025-02-18 15:43:31 -0400

    fix main.js bugs

--- components/Main.js
+++ components/Main.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebook, faInstagram, faGithub, faTiktok, faWhatsapp, faXTwitter } from '@fortawesome/free-brands-svg-icons';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-
-export function AccordionDemo() {
-  return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  )
-}
-
+import { faTwitter, faFacebook, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Main = (props) => (
     <div id="main" style={props.timeout ? { display: 'flex' } : { display: 'none' }}>
@@ -57,14 +24,14 @@ const Main = (props) => (
         </article>
 
         <article id="about" className={`${props.article === 'about' ? 'active' : ''} ${props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
+          <h2 className="major">Calculadora</h2>
           <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
           <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
           <div className="close" onClick={() => { props.onCloseArticle() }}></div>
         </article>
 
         <article id="contact" className={`${props.article === 'contact' ? 'active' : ''} ${props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-          <h2 className="major">Contact</h2>
+          <h2 className="major">Contacto</h2>
           <form method="post" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
@@ -85,7 +52,7 @@ const Main = (props) => (
           </form>
           <ul className="icons">
             <li><a href="#" onClick={() => { /* Manejar click */ }}>
-              <FontAwesomeIcon icon={faXTwitter} />
+              <FontAwesomeIcon icon={faTwitter} />
             </a></li>
             <li><a href="#" onClick={() => { /* Manejar click */ }}>
               <FontAwesomeIcon icon={faFacebook} />
@@ -94,10 +61,7 @@ const Main = (props) => (
               <FontAwesomeIcon icon={faInstagram} />
             </a></li>
             <li><a href="#" onClick={() => { /* Manejar click */ }}>
-              <FontAwesomeIcon icon={faTiktok} />
-            </a></li>
-            <li><a href="#" onClick={() => { /* Manejar click */ }}>
-              <FontAwesomeIcon icon={faWhatsapp} />
+              <FontAwesomeIcon icon={faGithub} />
             </a></li>
           </ul>
           <div className="close" onClick={() => { props.onCloseArticle() }}></div>
